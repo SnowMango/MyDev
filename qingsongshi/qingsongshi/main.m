d62f4581ea3c608293401262371e53adddd341f1
Feng丶zheng <zhengfeng@zhengfengdeiMac.local>
2017-01-04 16:57:43 +0800

    my

---
+++ qingsongshi/qingsongshi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  qingsongshi
+//
+//  Created by zhengfeng on 17/1/4.
+//  Copyright © 2017年 zhengfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
